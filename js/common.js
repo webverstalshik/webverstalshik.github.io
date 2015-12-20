1e44cfbaf1b4551af7cd742f69189958ca844ca7
Artem Bednyuk <webverstalshik@gmail.com>
2015-12-20 15:02:33 +0300

    second commit

--- js/common.js
+++ js/common.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
 // Плавная прокрутка при переходе по якорю
- $('a[href*=#]').bind("click", function(e){
-  var anchor = $(this);
-  var name = anchor.attr('href').replace(new RegExp("#",'gi'), '');
-  $('html, body').stop().animate({
-    scrollTop: $('a[name='+name+']').offset().top}, 1000);
-  e.preventDefault();
-  return false;
+//  $('a[href*=#]').bind("click", function(e){
+//   var anchor = $(this);
+//   var name = anchor.attr('href').replace(new RegExp("#",'gi'), '');
+//   $('html, body').stop().animate({
+//     scrollTop: $('a[name='+name+']').offset().top}, 1000);
+//   e.preventDefault();
+//   return false;
+// });
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() >= 600) {
+ 
+$('.btn-to-up').fadeIn('slow');
+ 
+} else {
+ 
+$('.btn-to-up').fadeOut('slow');
+ 
+}
+ 
 });
+ 
+$('.btn-to-up').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+}); 
 });
